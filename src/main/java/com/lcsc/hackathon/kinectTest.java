674e2c7b8c74b54b1d88795a5aa44ff3313edeca
jlrogers1 <jlrogers1@lcmail.lcsc.edu>
2015-02-07 15:18:29 -0800

    KinectTest update

--- src/main/java/com/lcsc/hackathon/kinectTest.java
+++ src/main/java/com/lcsc/hackathon/kinectTest.java
@@ -1,9 +1,45 @@
 package com.lcsc.hackathon;
 
-public class KinectTest {
+import edu.ufl.digitalworlds.j4k.J4KSDK; 
+import edu.ufl.digitalworlds.j4k.DepthMap; 
+import edu.ufl.digitalworlds.j4k.Skeleton; 
+import edu.ufl.digitalworlds.j4k.VideoFrame; 
 
+public class KinectTest extends J4KSDK {
+
+	VideoFrame videoTexture;
+	
+	public KinectTest() {
+		super();
+		videoTexture = new VideoFrame();
+	}
+	
+    @Override 
+    public void onDepthFrameEvent(short[] depth_frame, byte[] player_index, float[] XYZ, float[] UV) { 
+         
+        DepthMap map=new DepthMap(getDepthWidth(),getDepthHeight(),XYZ); 
+        if(UV!=null) map.setUV(UV);
+        System.out.println("DepthFrameEvent");
+    } 
+
+    @Override 
+    public void onSkeletonFrameEvent(boolean[] skeleton_tracked, float[] joint_position, float[] joint_orientation, byte[] joint_status) { 
+
+        Skeleton skeletons[]=new Skeleton[getMaxNumberOfSkeletons()]; 
+        for(int i=0;i<getMaxNumberOfSkeletons();i++) 
+        	skeletons[i]=Skeleton.getSkeleton(i, skeleton_tracked, joint_position, joint_orientation, joint_status, this);    
+        System.out.println("SkeletonFrameEvent");
+    } 
+	
+    @Override 
+    public void onColorFrameEvent(byte[] data) {     
+
+        videoTexture.update(getColorWidth(), getColorHeight(), data); 
+        System.out.println("ColorFrameEvent");
+    }
+    
 	public static void main(String[] args) {
-		System.out.println("This is a test");
+		System.out.println("Main");
 	}
 
 }
