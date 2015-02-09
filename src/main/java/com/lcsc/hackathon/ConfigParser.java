6f5635a99e0dff31462ae68eabbe5f5defc0f5cd
JakeWaffle <jawaffle@lcmail.lcsc.edu>
2015-02-09 12:00:59 -0800

    Added a license to the program.

--- src/main/java/com/lcsc/hackathon/ConfigParser.java
+++ src/main/java/com/lcsc/hackathon/ConfigParser.java
@@ -1,3 +1,36 @@
+/*
+This program is called "Kinect Controller". It is meant to detect gestures with the Kinect
+and then simulate keyboard and/or mouse input. The configuration files used by this program are
+not intended to be under the following license.
+
+The Kinect Controller makes use of the J4K library and Esper and we have done
+nothing to change their source.
+
+By using J4K we are required to site their research article:
+A. Barmpoutis. 'Tensor Body: Real-time Reconstruction of the Human Body and Avatar Synthesis from RGB-D',
+IEEE Transactions on Cybernetics, Special issue on Computer Vision for RGB-D Sensors: Kinect and Its
+Applications, October 2013, Vol. 43(5), Pages: 1347-1356.
+
+By using Esper without their commercial license we are also required to release our software under
+a GPL license.
+
+Copyright (C) 2015  Jacob Waffle, Ryan Spiekerman and Josh Rogers
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 package com.lcsc.hackathon;
 
 import com.lcsc.hackathon.events.*;
@@ -20,6 +53,21 @@ import java.io.FileNotFoundException;
 
 import org.eclipse.jetty.util.ajax.JSON;
 import org.apache.log4j.Logger;
+
+/*
+    Important Note:
+        This class should be restructured because it doesn't work as good as it could.
+        It should in general be used to retrieve specific parts of the configuration file
+        and then convert them into a map for use. Then based off of the rules and triggers
+        it should be delegating responsibility to another class to deal with those rules
+        and triggers in order to setup the program.
+        
+        The KeyPress and MouseMove triggers both required a different setup that couldn't easily
+        be factored into the logic for this class. The setup is better off being executed by different
+        classes for each of them. There also needs to be the ability to add custom logic into the
+        configuration file. So that'll have to be figured out also.
+*/
+
 
 public class ConfigParser {
     static Logger log = Logger.getRootLogger();
