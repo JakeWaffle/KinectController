38f0c4e95a75ff75d9747c29188aea2e274f9211
Jake Waffle <jawaffle@lcmail.lcsc.edu>
2015-10-30 00:03:23 -0700

    Updated the license header for the source files (j4k was removed from the project.) Also added a copy of the GPL2 license to the project.

--- src/main/java/com/lcsc/hackathon/kinectcontroller/emulation/reactions/ButtonReaction.java
+++ src/main/java/com/lcsc/hackathon/kinectcontroller/emulation/reactions/ButtonReaction.java
@@ -3,14 +3,6 @@ This program is called "Kinect Controller". It is meant to detect gestures with 
 and then simulate keyboard and/or mouse input. The configuration files used by this program are
 not intended to be under the following license.
 
-The Kinect Controller makes use of the J4K library and Esper and we have done
-nothing to change their source.
-
-By using J4K we are required to site their research article:
-A. Barmpoutis. 'Tensor Body: Real-time Reconstruction of the Human Body and Avatar Synthesis from RGB-D',
-IEEE Transactions on Cybernetics, Special issue on Computer Vision for RGB-D Sensors: Kinect and Its
-Applications, October 2013, Vol. 43(5), Pages: 1347-1356.
-
 By using Esper without their commercial license we are also required to release our software under
 a GPL license.
 
@@ -30,7 +22,6 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
-
 package com.lcsc.hackathon.kinectcontroller.emulation.reactions;
 
 import com.lcsc.hackathon.kinectcontroller.emulation.reactions.config.ReactionConfig;
